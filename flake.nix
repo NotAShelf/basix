aa5fc67d1542beabb68f88c11b48b5eb8fbe05d3
NotAShelf <raf@notashelf.dev>
2026-09-04 17:23:53 +0300

    flake: drop flake-parts
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I5077e4edb0acd964234d24a8ba9967266a6a6964

--- flake.nix
+++ flake.nix
@@ -3,90 +3,74 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = {
-    flake-parts,
-    self,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} (let
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
 
-      # FWIW this could also be lib.extend but lib.extend itself claims that
-      # it should not be used, so we do this instead.
-      inherit (inputs.nixpkgs) lib;
-      basixLib = import ./lib.nix {inherit lib;};
+    # FWIW this could also be lib.extend but lib.extend itself claims that
+    # it should not be used, so we do this instead.
+    nixpkgsLib = nixpkgs.lib;
+    basixLib = import ./lib.nix {lib = nixpkgsLib;};
 
-      mkThemeAttrSet = pkgs: schemes: let
-        mkGtkTheme = pkgs.callPackage ./packages/gtk/package.nix {basixLib = self.lib;};
-        mkQtctTheme = pkgs.callPackage ./packages/qtct/package.nix {basixLib = self.lib;};
-        mkKvantumTheme = pkgs.callPackage ./packages/kvantum/package.nix {basixLib = self.lib;};
-      in
-        lib.mapAttrs (slug: scheme:
-          pkgs.symlinkJoin {
-            name = "basix-theme-${self.lib.sanitizeSlug slug}";
-            paths = [
-              (mkGtkTheme {inherit slug scheme;})
-              (mkQtctTheme {inherit slug scheme;})
-              (mkKvantumTheme {inherit slug scheme;})
-            ];
-          })
-        schemes;
+    schemeData = {
+      base16 = basixLib.evalSchemeData ./json/base16;
+      base24 = basixLib.evalSchemeData ./json/base24;
+    };
 
-      mkSystemThemePackages = system: let
-        pkgs = import inputs.nixpkgs {inherit system;};
-      in {
-        base16 = mkThemeAttrSet pkgs self.schemeData.base16;
-        base24 = mkThemeAttrSet pkgs self.schemeData.base24;
-      };
+    pkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+
+    mkThemeAttrSet = pkgs: schemes: let
+      mkGtkTheme = pkgs.callPackage ./packages/gtk/package.nix {inherit basixLib;};
+      mkQtctTheme = pkgs.callPackage ./packages/qtct/package.nix {inherit basixLib;};
+      mkKvantumTheme = pkgs.callPackage ./packages/kvantum/package.nix {inherit basixLib;};
+    in
+      nixpkgsLib.mapAttrs (slug: scheme:
+        pkgs.symlinkJoin {
+          name = "basix-theme-${basixLib.sanitizeSlug slug}";
+          paths = [
+            (mkGtkTheme {inherit slug scheme;})
+            (mkQtctTheme {inherit slug scheme;})
+            (mkKvantumTheme {inherit slug scheme;})
+          ];
+        })
+      schemes;
+
+    themePackages = forAllSystems (system: {
+      base16 = mkThemeAttrSet pkgsFor.${system} schemeData.base16;
+      base24 = mkThemeAttrSet pkgsFor.${system} schemeData.base24;
+    });
+
+    packages = forAllSystems (system: let
+      pkgs = pkgsFor.${system};
+      perSystemThemes = themePackages.${system};
     in {
-      inherit systems;
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        themePackages = self.themePackages.${system};
-      in {
-        packages = {
-          # Converts YAML -> JSON
-          convert-scheme = pkgs.callPackage ./packages/convert-scheme/package.nix {};
+      # Converts YAML -> JSON
+      convert-scheme = pkgs.callPackage ./packages/convert-scheme/package.nix {};
 
-          # Theme collections
-          themes-base16 = pkgs.symlinkJoin {
-            name = "basix-themes-base16";
-            paths = lib.attrValues themePackages.base16;
-          };
-
-          themes-base24 = pkgs.symlinkJoin {
-            name = "basix-themes-base24";
-            paths = lib.attrValues themePackages.base24;
-          };
-
-          themes-all = pkgs.symlinkJoin {
-            name = "basix-themes-all-${system}";
-            paths = (lib.attrValues themePackages.base16) ++ (lib.attrValues themePackages.base24);
-          };
-        };
+      # Theme collections
+      themes-base16 = pkgs.symlinkJoin {
+        name = "basix-themes-base16";
+        paths = nixpkgsLib.attrValues perSystemThemes.base16;
       };
 
-      flake = let
-        inherit (basixLib) evalSchemeData;
-      in {
-        lib = basixLib;
+      themes-base24 = pkgs.symlinkJoin {
+        name = "basix-themes-base24";
+        paths = nixpkgsLib.attrValues perSystemThemes.base24;
+      };
 
-        schemeData = {
-          base16 = evalSchemeData "${self}/json/base16";
-          base24 = evalSchemeData "${self}/json/base24";
-        };
-
-        themePackages = lib.genAttrs systems mkSystemThemePackages;
+      themes-all = pkgs.symlinkJoin {
+        name = "basix-themes-all-${system}";
+        paths = (nixpkgsLib.attrValues perSystemThemes.base16) ++ (nixpkgsLib.attrValues perSystemThemes.base24);
       };
     });
+  in {
+    inherit schemeData themePackages packages;
+    lib = basixLib;
+  };
 }
